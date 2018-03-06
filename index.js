34529f43629de91a1aa177069cfeac67e6b9a154
wley3337@gmail.com <wley3337@gmail.com>
2018-03-06 12:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
+// destructivelyAppendKitten(name) appends a kitten to end of the Array
+
+function destructivelyAppendKitten(name){
+  kittens.pop();
+}
+
+//destructivelyPrependKitten(name) beginning of the Array
+
+//destructivelyRemoveLastKitten() removes the last kitten from the kittens Array
+
+//destructivelyRemoveFirstKitten() removes the first kittens
+
+//appendKitten(name) apends a kitten to the kitten array, returns new arry, leaves kittens array unchanged
+
+// prependKitten(name) prepends a kitten array returns new array, leaving the kitten array unchanged
+
+// removeLastKitten() removes last kitten in the array, returns new array, levain kittens unchanged
+
+// removeFirstKitten() removes the first kitten from the kittens array and returns a new array
